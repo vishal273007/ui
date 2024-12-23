2a2f29feb64b2f2f500e9493387f4732112c7c9d
Vishal Vishwakarma <vv273007@gmail.com>
2024-12-23 21:52:22 +0530

    updaeted folders

--- 03_JavaScript/code/05_operators/scripts.js
+++ 03_JavaScript/code/05_operators/scripts.js
@@ -11,15 +11,91 @@
 
 // Relational operators:
 
-let a = 20, b = 5, c = '20';
-console.log(a > b); //T
-console.log(a < b); //F
-console.log(a >= b); //T
-console.log(a <= b); //F
-console.log(a == c); //T
-console.log(a === c); //F
-console.log(a != c); //F
-console.log(a !== c); //F
+// let a = 20, b = 5, c = '20';
+// console.log(a > b); //T
+// console.log(a < b); //F
+// console.log(a >= b); //T
+// console.log(a <= b); //F
+// console.log(a == c); //T
+// console.log(a === c); //F
+// console.log(a != c); //F
+// console.log(a !== c); //F
+
+
+
+// Logical Operators
+
+let a = 20, b = 10;
+console.log("logical AND will be true when both statements are true " + ((a > 2) && (b > 3)));
+console.log("logical OR will be true when one is statements true true " + ((b > 2) || (b > 30)));
+console.log("logical NOT will be true when statements is false " + !(a > 20));
+
+
+
+// Assignment or Shorthand operators
+
+// console.log(a = 2);
+
+console.log(a += 2);
+console.log(a -= 2);
+console.log(a *= 2);
+console.log(a /= 2);
+console.log(a **= 2); // a =  a pwr 2
+console.log(a %= 2);
+
+
+
+a = 5;
+b = a++; // a=a+1
+
+console.log(a++);
+console.log(++a);
+
+c = ++a;
+console.log(a, b, c);
+
+a = 20, b = 10;
+c = ++a + a++ + --b + b--; //21 + 21 + 9 + 9
+console.log(c);
+
+
+// Ternary Operators  (Most important)
+
+a = 10;
+// a = 11;
+c = (a % 2 == 0) ? 'Even' : 'Odd';
+console.log(c);
+
+console.log('5' + 1); //51
+console.log(+'5' + 1); //6
+
+
+
+// coelsion operator
+a = 5;
+var d = 25;
+c = d ?? a;
+console.log(c);
+
+
+
+
+
+d = undefined;
+a = 10
+c = d ?? a;
+
+console.log(c);
+
+
+
+
+
+
+
+
+
+
 
 
 
